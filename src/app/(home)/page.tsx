068b4ae4b7605c7ee8bfeeec879bb3c9d1f6c856
OmiDev <omarblanco54@gmail.com>
2024-05-18 17:17:40 -0400

    implement route grouping to improve the loading's behavior

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,8 @@
-import { Description } from "app/presentation/home/components/Description";
-import { Hero } from "app/presentation/home/components/Hero";
 import { MainProducts } from "app/presentation/home/components/MainProducts";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <Description />
       <MainProducts />
     </main>
   );
